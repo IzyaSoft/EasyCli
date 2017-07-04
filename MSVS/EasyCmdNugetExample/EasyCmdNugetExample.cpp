21e5e6d20f3a00cff5062bef36305dc5354d8d60
Michael Ushakov <um.nix.user@gmail.com>
2017-07-04 14:23:03 +0400

    Example with cmd emulation

--- MSVS/EasyCmdNugetExample/EasyCmdNugetExample.cpp
+++ MSVS/EasyCmdNugetExample/EasyCmdNugetExample.cpp
@@ -1,11 +1,25 @@
-#include "windows.h"
-#include "tchar.h"
-
+#include <vector>
+#include <iostream>
 #include "CommandLineOptionsManager.h"
 
-
-int _tmain(int argc, char* argv[])
+int main(int argc, char **argv)
 {
-	return 0;
+    int wait;
+    // extra values tests
+    std::vector<char*> args = {"C:\apps\EasyCmdNugetExample.exe", "-d", "COM1", "-s", "115200", "-p", "onebit", "--verbose" };
+    EasySoftCmdLine::CommandLineOptionsManager cmdManager(new EasySoftCmdLine::ICommandLineOptionValidator(),
+                                                          args.size(), (const char**)args.data(), true, true);
+    
+    std::string value;
+    cmdManager.TryGetValue("-d", value);
+    std::cout << "VALUE FOR KEY -d :" << value.c_str() << std::endl;
+    cmdManager.TryGetValue("--s", value);
+    std::cout << "VALUE FOR KEY -s :" << value.c_str() << std::endl;
+    cmdManager.TryGetValue("-p", value);
+    std::cout << "VALUE FOR KEY -p :" << value.c_str() << std::endl;
+    
+    //std::vector<EasySoftCmdLine::KeyValueOption> options = cmdManager.GetParsedOptions();
+    std::cin >> wait;
+    return 0;
 }
 
